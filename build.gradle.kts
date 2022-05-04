51eb70a4170569ada1d0e698acadc7e8fc634950
Francisco J Lopez-Pellicer <fjlopez@unizar.es>
2022-05-04 22:22:49 +0200

    ci: setup sonarcloud

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     kotlin("jvm") version "1.6.20" apply false
     id("org.jlleitschuh.gradle.ktlint") version "10.2.1"
     id("org.jetbrains.kotlinx.kover") version "0.5.0"
+    id("org.sonarqube") version "3.3"
 }
 
 allprojects {
@@ -21,4 +22,10 @@ subprojects {
 
 ktlint {
     version.set("0.45.2")
+}
+
+sonarqube {
+    properties {
+        property("sonar.projectKey", "IAAA-Lab_frontier")
+    }
 }
